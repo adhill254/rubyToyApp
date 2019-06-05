c937c3ff42eaa5df4065f546c5082405a92b705a
Andrew Hill <ahill@rocketcyber.com>
2019-06-05 13:27:04 -0500

    Getting to 2.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name, presence: true
+  # validates :email, format: { :with "/\w+@\w+\.((com)|(edu)|(gov)|(co)|(ufop))/" }
+  validates_format_of :email,:with => /\A\w+@\w+\.\w+\z/
 end
